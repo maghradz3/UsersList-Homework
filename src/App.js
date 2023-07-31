fdee9b1e19bc3ac241c814e64edc82cad00bbaef
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-07-31 22:21:40 +0400

    change some logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { IcontButton } from "./components/IcontButton";
 import classes from "./App.module.css";
 
+
 function App() {
   return (
     <div className={classes.mainCont}>
       <IcontButton />
+      {/* <RandomIcon/> */}
     </div>
   );
 }
--- src/components/IcontButton.js
+++ src/components/IcontButton.js
@@ -1,62 +1,29 @@
-import React, { useEffect, useState } from "react";
-// import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faBalanceScale,
-  faCar,
-  faTrain,
-  faBaby,
-  fa0,
-  faRandom,
-  fa2,
-  faDog,
-  faPlane,
-  faClock,
-  faCircle,
-} from "@fortawesome/free-solid-svg-icons";
-// import * as icons from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import classes from "./IconButton.module.css";
+import React,{useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRandom, fas } from '@fortawesome/free-solid-svg-icons'
+import classes from "./IconButton.module.css"
 
 // library.add(fas, fa0, faCar, faTrain, faBalanceScale, faBaby);
-const fontIcons = [
-  faBalanceScale,
-  faCar,
-  faTrain,
-  faBaby,
-  fa0,
-  fa2,
-  faDog,
-  faPlane,
-  faClock,
-  faCircle,
-];
-console.log(fontIcons.length);
 
-// console.log(icons);
-// const iconsKeys = Object.keys(icons);
-// console.log(iconsKeys[0]);
 
-const getRandomIndex = () => {
-  const randomIndex = Math.floor(Math.random() * fontIcons.length);
-
-  return fontIcons[randomIndex];
-};
 
 export const IcontButton = () => {
-  const [fontIcon, setFontIcon] = useState(faRandom);
+  const [fontIcon, setFontIcon] = useState(null);
 
   const generateIconHandler = () => {
-    const timer = setTimeout(() => {
-      setFontIcon(getRandomIndex());
+    setTimeout(() => {
+      const iconsArray=Object.keys(fas)
+      const randomIcon=iconsArray[Math.floor(Math.random()*iconsArray.length)]
+      setFontIcon(randomIcon);
     }, 3000);
-    return timer;
+ 
   };
 
   return (
     <div className={classes.contentWrapper}>
-      <FontAwesomeIcon icon={fontIcon} color="wheat" fontSize="40px" />
+      {fontIcon&&<FontAwesomeIcon icon={fas[fontIcon]} color="wheat" fontSize="40px" />}
       <button className={classes.Button} onClick={generateIconHandler}>
-        Generate <FontAwesomeIcon icon={fontIcon} />
+        Generate Icon {fontIcon&&<FontAwesomeIcon icon={fas[fontIcon]} />}
       </button>
     </div>
   );
