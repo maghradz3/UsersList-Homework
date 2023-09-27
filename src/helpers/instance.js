89d67cb24628d274e9bdd5f901764be920731c5a
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-09-27 18:57:19 +0400

    solved link from .env

--- src/helpers/instance.js
+++ src/helpers/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const backendUrl = process.env.REACT_APP_BA;
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 export const axiosInstance = axios.create({
   baseURL: backendUrl,
